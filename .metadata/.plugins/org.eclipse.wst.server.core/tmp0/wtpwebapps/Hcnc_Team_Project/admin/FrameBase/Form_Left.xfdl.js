0f63da2b04bc97ebb1e1d9dbcd2889c9bb735976
hankil <hankil920602@gmail.com>
2025-09-09 16:01:03 +0900

    metadata test

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Hcnc_Team_Project/admin/FrameBase/Form_Left.xfdl.js
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Hcnc_Team_Project/admin/FrameBase/Form_Left.xfdl.js
@@ -9,30 +9,31 @@
         
         this.on_create = function()
         {
-            this.set_scrollbartype("none");
-            this.set_scrolltype("none");
             this.set_name("Form_Left");
-            this.set_titletext("New Form");
+            this.set_titletext("Form_Left");
             if (Form == this.constructor)
             {
-                this._setFormPosition(200,720);
+                this._setFormPosition(200,570);
             }
             
             // Object(Dataset, ExcelExportObject) Initialize
-            obj = new Dataset("dsLeft", this);
-            obj._setContents("<ColumnInfo><Column id=\"MENU_ID\" type=\"STRING\" size=\"256\"/><Column id=\"MENU_NM\" type=\"STRING\" size=\"256\"/><Column id=\"MENU_LEVEL\" type=\"STRING\" size=\"256\"/><Column id=\"MENU_PATH\" type=\"STRING\" size=\"256\"/></ColumnInfo><Rows><Row/></Rows>");
+            obj = new Dataset("ds_left", this);
+            obj._setContents("<ColumnInfo><Column id=\"MENU_ID\" type=\"STRING\" size=\"256\"/><Column id=\"MENU_NM\" type=\"STRING\" size=\"256\"/><Column id=\"MENU_LEVEL\" type=\"INT\" size=\"10\"/><Column id=\"MENU_PATH\" type=\"STRING\" size=\"256\"/><Column id=\"CSSCLASS\" type=\"STRING\" size=\"256\"/></ColumnInfo><Rows><Row/></Rows>");
             this.addChild(obj.name, obj);
             
             // UI Components Initialize
-            obj = new Grid("Grid00","0","0",null,null,"0","0",null,null,null,null,this);
+            obj = new Grid("Grid00","-2","-10","204","576",null,null,null,null,null,null,this);
             obj.set_taborder("0");
-            obj.set_binddataset("dsLeft");
-            obj._setContents("<Formats><Format id=\"default\"><Columns><Column size=\"200\"/></Columns><Rows><Row size=\"24\"/></Rows><Band id=\"body\"><Cell treelevel=\"bind:MENU_LEVEL\" treestartlevel=\"1\" text=\"bind:MENU_NM\" displaytype=\"normal\" edittype=\"none\"/></Band></Format></Formats>");
+            obj.set_binddataset("ds_left");
+            obj.set_treeusebutton("no");
+            obj.set_treeusecheckbox("false");
+            obj.set_treeuseline("false");
+            obj.set_treeuseimage("true");
+            obj._setContents("<Formats><Format id=\"default\"><Columns><Column size=\"206\"/></Columns><Rows><Row size=\"24\" band=\"head\"/><Row size=\"41\"/></Rows><Band id=\"head\"><Cell text=\"MENU_NM\"/></Band><Band id=\"body\"><Cell text=\"bind:MENU_NM\" displaytype=\"treeitemcontrol\" edittype=\"tree\" treelevel=\"bind:MENU_LEVEL\" cssclass=\"bind:CSSCLASS\"/></Band></Format></Formats>");
             this.addChild(obj.name, obj);
             // Layout Functions
             //-- Default Layout : this
-            obj = new Layout("default","",200,720,this,function(p){});
-            obj.set_mobileorientation("landscape");
+            obj = new Layout("default","Desktop_screen",200,570,this,function(p){});
             this.addLayout(obj.name, obj);
             
             // BindItem Information
@@ -49,57 +50,93 @@
         
         // User Script
         this.registerScript("Form_Left.xfdl", function() {
-
-        this.Form_Left_onload = function(obj,e)
+        this.Form_Left_onload = function(obj, e)
         {
+            trace("Form_Left_onload 실행됨");
+            this.fnGetLeftMenu();   // 좌측 메뉴 초기 로드
+        	trace(this.ds_left.saveXML());
 
         };
 
-        this.fnGetLeftMenu = function(sMenuId){
-        	var objApp = nexacro.getApplication();
-        	var objDsMenu = objApp.gds_headerInfo;
-
-        	objDsMenu.filter("MENU_ID.indexOf('"+ sMenuId +"') == 0 && MENU_LEVEL > 0");
-        	this.dsLeft.copyData(objDsMenu, true);
-        	objDsMenu.filter("");
-        }
-
-
-        this.Grid00_oncellclick = function(obj,e)
+        // 좌측 메뉴 데이터 로드 (TreeLevel 세팅)
+        this.fnGetLeftMenu = function()
         {
-        	var objApp = nexacro.getApplication();
-        	var objOpenMenu = objApp.gds_open;
-        	var objMenu = obj.getBindDataset();
-        	var sMenuId = objMenu.getColumn(e.row, "MENU_ID");
+            var objApp    = nexacro.getApplication();
+            var objDsMenu = objApp.gds_menu;
 
+            // 모든 메뉴 가져오기 (LEVEL 있는 상태)
+            this.ds_left.copyData(objDsMenu, true);
 
-        	if(sMenuId.length == 4){
+            // 트리 뷰어용 TreeLevel 컬럼 값 강제 세팅
+          for (var i = 0; i < this.ds_left.getRowCount(); i++) {
+        		var sMenuId = this.ds_left.getColumn(i, "MENU_ID");
 
-        		var RowTree = this.Grid00.getTreeRow(e.row);
-        		var Status  = this.Grid00.getTreeStatus(RowTree);
-
-        		Status = (Status == 0 ? 1 : 0);
-        		this.Grid00.setTreeStatus(RowTree, 0);
-
-        	} else if(sMenuId.length == 6){
-        		this.fnOpenMenu(sMenuId);
-        	} else {
-        		this.alert('오류가 발생하였습니다');
+        		if (sMenuId.length == 2) {
+        			this.ds_left.setColumn(i, "MENU_LEVEL", 0);   // 루트
+        			this.ds_left.setColumn(i, "CSSCLASS", "menuRoot"+i); // i값으로 cssclass 세팅
+        			 objButton.set_cssclass(sBtnCssClass);
+        		} else if (sMenuId.length == 4) {
+        			this.ds_left.setColumn(i, "MENU_LEVEL", 1);   // 자식
+        			this.ds_left.setColumn(i, "CSSCLASS", "menuChild");    // 자식은 공통 클래스
+        		}
         	}
+
         };
 
-        this.fnOpenMenu = function(sMenuId){
-        	var objApp = nexacro.getApplication();
-        	var objDsMenu = this.dsLeft;
+        // 그리드 셀 클릭 이벤트
+        this.Grid00_oncellclick = function(obj, e)
+        {
+            var sMenuId = this.ds_left.getColumn(e.row, "MENU_ID");
 
-        	var objDsOpenMenu = objApp.gds_headerInfo;
+            // 부모 (2자리)
+            if (sMenuId.length == 2) {
+                // 현재 row 기준으로 자식 있는지 검사
+                var hasChild = false;
+                for (var i = 0; i < this.ds_left.getRowCount(); i++) {
+                    var childId = this.ds_left.getColumn(i, "MENU_ID");
+                    if (childId.indexOf(sMenuId) == 0 && childId.length == 4) {
+                        hasChild = true;
+                        break;
+                    }
+                }
 
-        	var nFRow = objDsMenu.findRow("MENU_ID", sMenuId);
-        	var sMenuPath = objDsMenu.getColumn(nFRow,"MENU_PATH");
+                if (hasChild) {
+                    // 트리 토글
+                    var rowTree   = obj.getTreeRow(e.row);
+                    var currState = obj.getTreeStatus(rowTree);
 
-        	nexacro.setPrivateProfile("originPath",sMenuPath);
-        	objApp.mainframe.VFrameSet00.HFrameSet00.MainFrame.set_formurl(sMenuPath)
-        }
+                    if (currState != 3) {
+                        var newState = (currState == 0 ? 1 : 0);
+                        obj.setTreeStatus(rowTree, newState);
+                    }
+                } else {
+                    // 자식이 없으면 바로 메뉴 실행
+                    this.fnOpenMenu(sMenuId);
+                }
+            }
+            // 자식 (4자리) → 화면 실행
+            else if (sMenuId.length == 4) {
+                this.fnOpenMenu(sMenuId);
+            }
+        };
+
+
+
+        // 메뉴 실행
+        this.fnOpenMenu = function(sMenuId)
+        {
+            var objApp = nexacro.getApplication();
+            var nFRow  = this.ds_left.findRow("MENU_ID", sMenuId);
+            if (nFRow < 0) return;
+
+            var sMenuPath = this.ds_left.getColumn(nFRow, "MENU_PATH");
+            if (!sMenuPath || sMenuPath == "[Undefined]") {
+                trace("메뉴 경로가 정의되지 않음: " + sMenuId);
+                return;
+            }
+
+            objApp.mainframe.VFrameSet00.HFrameSet00.WorkFrame.set_formurl(sMenuPath);
+        };
 
         });
         
--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Hcnc_Team_Project/admin/environment.xml.js
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Hcnc_Team_Project/admin/environment.xml.js
@@ -21,7 +21,14 @@ if (nexacro.Environment)
         nexacro._addService("font", "file", "./_resource_/_font_/", "session", null, "", "0", "");
         nexacro._addService("Base", "form", "./Base/", "session", null, "", "0", "0");
         nexacro._addService("FrameBase", "form", "./FrameBase/", "session", null, "", "0", "0");
-        nexacro._addService("svc", "JSP", "http://localhost:8080", "none", null, "", "0", "0");
+        nexacro._addService("svc", "JSP", "http://localhost:8080/", "session", null, "", "0", "0");
+        nexacro._addService("order", "form", "./order/", "session", null, "", "0", "0");
+        nexacro._addService("board", "form", "./board/", "session", null, "", "0", "0");
+        nexacro._addService("promo", "form", "./promo/", "session", null, "", "0", "0");
+        nexacro._addService("member", "form", "./member/", "session", null, "", "0", "0");
+        nexacro._addService("product", "form", "./product/", "session", null, "", "0", "0");
+        nexacro._addService("stat", "form", "./stat/", "session", null, "", "0", "0");
+        nexacro._addService("setting", "form", "./setting/", "session", null, "", "0", "0");
     	nexacro._component_uri = (nexacro._arg_compurl ? nexacro._arg_compurl : "./nexacro17lib/component/");
     	nexacro._theme_uri = "./_resource_/_theme_/";
     	// load components
