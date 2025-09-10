a7039c8c3ff06d6df5efa07f3550b366ddb1a8c5
hankiljung <hankil920602@gmail.com>
2025-09-10 17:00:33 +0900

    09.10 Order 서비스, 컨트롤러, 매퍼 생성

---
+++ Hcnc_Team_Project/src/main/java/admin/service/OrderService.java
@@ -0,0 +1,19 @@
+package admin.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import admin.mapper.OrderMapper;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderMapper orderMapper;
+	
+
+	
+}
