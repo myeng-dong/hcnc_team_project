5a3df4ed94e4c2e4d6aa3b02056ac7883d137450
myeng-dong <wpvos456@gmail.com>
2025-09-15 15:09:52 +0900

    [JSP] 아이디중복체크, 이메일인증 구현

--- Hcnc_Team_Project/src/main/java/user/web/UserMemberController.java
+++ Hcnc_Team_Project/src/main/java/user/web/UserMemberController.java
@@ -2,6 +2,7 @@ package user.web;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,11 +15,29 @@ import user.mail.UserMailService;
 import user.service.UserMemberService;
 
 @Controller
-@RequestMapping("/user") 
 public class UserMemberController {
 	@Autowired
 	private UserMemberService userMemberService;
+	@Autowired
 	private UserMailService mailService;
+	
+	public class CodeInfo {
+	    private int code;           // 인증번호
+	    private long createdTime;   // 발급 시간 (밀리초)
+
+	    public CodeInfo(int code, long createdTime) {
+	        this.code = code;
+	        this.createdTime = createdTime;
+	    }
+
+	    public int getCode() {
+	        return code;
+	    }
+
+	    public long getCreatedTime() {
+	        return createdTime;
+	    }
+	}
 	
 	@Data
 	public class SignupForm {
@@ -31,12 +50,27 @@ public class UserMemberController {
 		private String bitrh;
 		private String gender;
 	}
-
 	
-	@RequestMapping("/login")
-	public ModelAndView insertSignUpByUser () {
+	private static final Map<String, CodeInfo> emailAuthMap = new ConcurrentHashMap();
+	
+	public void saveAuthCode(String email, int code) {
+	    emailAuthMap.put(email, new CodeInfo(code, System.currentTimeMillis()));
+	}
+	
+	public boolean verifyAuthCode(String email, int inputCode) {
+	    CodeInfo info = emailAuthMap.get(email);
+	    return info != null && info.getCode() == inputCode;
+	}
+	
+	public static int RandomCode () {
+		int number = (int)(Math.random() * 900000) + 100000;
+		return number;
+	}
+	
+	@RequestMapping("/sign.do")
+	public ModelAndView signUser () {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("sign/login.html");
+		mv.setViewName("sign/sign-up");
 		return mv;
 	}
 
@@ -53,6 +87,7 @@ public class UserMemberController {
 		mv.setViewName("jsonView");
 		try{
 			int idCnt = userMemberService.selectIdCheckByUser(id);
+			mv.addObject("count", idCnt);
 			if(idCnt == 0){
 				mv.addObject("status", 200);
 			} else {
@@ -66,26 +101,39 @@ public class UserMemberController {
 
 	@RequestMapping("/selectEmailCheckByUser.do")
 	public ModelAndView selectEmailCheckByUser(
-	        @RequestParam("to") String to,
-	        @RequestParam("subject") String subject,
-	        @RequestParam("text") String text
+	        @RequestParam("to") String to
         ) {
 	    ModelAndView mv = new ModelAndView("mailResult");
 	    mv.setViewName("jsonView");
 	    try {
 			int emailChk = userMemberService.selectEmailCheckByUser(to);
+			System.out.println(to);
+			System.out.println(emailChk == 0);
 			if(emailChk == 0){
-	    		mailService.sendMail(to, subject, text);	
+				String subject = "DDD.D 이메일 인증번호";
+				String text = "DDD.D 이메일 인증번호: ";
+				int code = RandomCode();
+				saveAuthCode(to,code);
+	    		mailService.sendMail(to, subject, text + code);	
 				mv.addObject("status", 200);
+				mv.addObject("to",to);
 			} else {
 				mv.addObject("status", 409);
 			}
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-	    
-	    mv.addObject("to", to);
-	    
 	    return mv;
+	}
+	
+	@RequestMapping("/selectVerifyAuthByUser.do")
+	public ModelAndView selectVerifyAuthByUser(
+			@RequestParam("to") String to,
+			@RequestParam("code") int code
+			) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("jsonView");
+		mv.addObject("result", verifyAuthCode(to,code)) ;
+		return mv;
 	}
 }
