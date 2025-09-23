bb6b677cc8a5a35d1f1ce2ee9ae9dc993d94eea0
Lin-Bee <61338147+Lin-Bee@users.noreply.github.com>
2025-09-23 12:30:50 +0900

    0923 menu 내 카테고리 출력 : 스타일미작업
    0923 menu 내 카테고리 출력 : 스타일미작업

---
+++ Hcnc_Team_Project/src/main/java/user/web/UserGlobalAdvice.java
@@ -0,0 +1,37 @@
+package user.web;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import user.service.UserCategoryService;
+
+//공용일때 advice
+@ControllerAdvice
+public class UserGlobalAdvice {
+
+    @Autowired
+    private UserCategoryService userCategoryService;
+
+    @ModelAttribute("categories") 
+    public List<Map<String, Object>> getCategoryList() {
+
+        List<Map<String, Object>> mainCategories = userCategoryService.selectMainCategoryListByUser();
+        System.out.println("메인 카테고리 개수: " + mainCategories.size());
+        
+        for (Map<String, Object> main : mainCategories) {
+        	int mainCateId = ((Number) main.get("MAIN_CATE_ID")).intValue();
+            List<Map<String, Object>> subCategories = userCategoryService.selectSubCategoryListByUser(mainCateId);
+
+            if (subCategories == null) {
+                subCategories = new ArrayList<>();
+            }
+            main.put("subCategories", subCategories);
+            System.out.println("메인ID " + mainCateId + " -> 서브개수: " + subCategories.size());
+        }
+  
+        return mainCategories;
+    }
+}
