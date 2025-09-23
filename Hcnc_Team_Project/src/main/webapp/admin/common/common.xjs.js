58296aea1fd9f651bc30c10129340c97693cc77c
sunwhats <khr240722@gmail.com>
2025-09-23 16:53:43 +0900

    트랜잭션 url ( ?time=" + new Date().getTime(),) 자동적용 공통 xjs
    사용시  include "common::common.xjs" 를 스크립트  상단에 붙여주기

---
+++ Hcnc_Team_Project/src/main/webapp/admin/common/common.xjs.js
@@ -0,0 +1,38 @@
+//XJS=common.xjs
+(function()
+{
+    return function(path)
+    {
+        var obj;
+    
+        // User Script
+        this.registerScript(path, function() {
+        this.gfn_transction = function(rtnId, url, inDs, outDs, strVal){
+
+        	this.transaction(
+                rtnId,            // 서비스 ID (콜백 분기용)
+        		"svc::/"+url+"?time="+new Date().getTime(),    // 컨트롤러 URL
+                inDs,         // inDatasets: 조건 묶음
+                outDs,       // outDatasets: 결과 그리드 바인딩
+                strVal,                                    // args는 사용하지 않음 (전부 Dataset으로 처리)
+                "fn_callback",                         // 콜백 함수명
+                true                                   // 비동기
+            );
+
+
+        }
+
+        this.gfn_transction2 = function(){
+
+        	this.alert("!@@@!@!@!@!@!@");
+
+
+        }
+        });
+    
+        this.loadIncludeScript(path);
+        
+        obj = null;
+    };
+}
+)();
--- Hcnc_Team_Project/src/main/webapp/admin/environment.xml.js
+++ Hcnc_Team_Project/src/main/webapp/admin/environment.xml.js
@@ -32,6 +32,7 @@ if (nexacro.Environment)
         nexacro._addService("banner", "form", "./banner/", "session", null, "", "0", "0");
         nexacro._addService("dash", "form", "./dash/", "session", null, "", "0", "0");
         nexacro._addService("popup", "form", "./popup/", "session", null, "", "0", "0");
+        nexacro._addService("common", "js", "./common/", "session", null, "", "0", "0");
     	nexacro._component_uri = (nexacro._arg_compurl ? nexacro._arg_compurl : "./nexacro17lib/component/");
     	nexacro._theme_uri = "./_resource_/_theme_/";
     	// load components
