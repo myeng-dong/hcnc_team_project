0ea2d77d64b8f7602db6295fc70325fa90dd2173
myeng-dong <wpvos456@gmail.com>
2025-09-25 17:07:47 +0900

    [Nxacro] 상품 등록 edit추가

--- Hcnc_Team_Project/src/main/java/admin/web/ProductCreateController.java
+++ Hcnc_Team_Project/src/main/java/admin/web/ProductCreateController.java
@@ -9,7 +9,9 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest; // javax 유지
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,14 +31,28 @@ public class ProductCreateController {
 	@Autowired
 	ProductCreateService productCreateService;
 	
-	 @RequestMapping("/selectProductCategoryListByAdmin.do")
-	 public NexacroResult selectProductCategoryListByAdmin() {
-	     NexacroResult result = new NexacroResult();
-	     List<Map<String,Object>> categorys = productCreateService.selectProductCategoryListByAdmin();
-	     System.out.println(categorys);
-	     result.addDataSet("ds_cate_main",categorys);
-	     result.addDataSet("ds_cate_sub",categorys);
-	     return result;
-	 }
+	@RequestMapping(value="/selectProductCategoryListByAdmin.do", produces = "application/json; charset=utf-8")
+    public ResponseEntity<String> getCategories() throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper();
+		List<Map<String,Object>> categorys = productCreateService.selectProductCategoryListByAdmin();
+	   	System.out.println(objectMapper.writeValueAsString(categorys));
+        String jsonString = objectMapper.writeValueAsString(categorys);
+        
+        return ResponseEntity.ok(jsonString);
+    }
+	/*
+	 * @RequestMapping("/selectProductCategoryListByAdmin.do") public NexacroResult
+	 * selectProductCategoryListByAdmin() { NexacroResult result = new
+	 * NexacroResult(); ObjectMapper objectMapper = new ObjectMapper(); try {
+	 * List<Map<String,Object>> categorys =
+	 * productCreateService.selectProductCategoryListByAdmin();
+	 * System.out.println(objectMapper.writeValueAsString(categorys));
+	 * result.addDataSet("ds_cate_main",categorys); } catch (Exception e) {
+	 * 
+	 * }
+	 * 
+	 * 
+	 * result.add("ds_cate_sub",categorys); return result; }
+	 */
 	
 }
