012833fce9aecdcb27fca48855846cab5ae0a2e5
Hobbyer <uskimeunyeob@gmail.com>
2025-10-05 20:53:20 +0900

    [JSP] 리뷰 등록을 위한 프론트엔드, 백엔드 사전 준비 작업 완료(주문내역 구현 후 이어서 작업할 예정)

--- Hcnc_Team_Project/src/main/java/user/web/UserReviewController.java
+++ Hcnc_Team_Project/src/main/java/user/web/UserReviewController.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import user.service.UserReviewService;
@@ -17,6 +20,7 @@ public class UserReviewController {
 
 	@Autowired
 	private UserReviewService userReviewService;
+	
 	
 	@RequestMapping(value="/selectReviewList.do")
 	public ModelAndView selectReviewListByUser(@RequestParam Map<String, Object> param) {
@@ -75,6 +79,55 @@ public class UserReviewController {
 		} catch(Exception e) {
 			mav.addObject("error", "리뷰 조회 중 오류발생");
 		}
+		
+		return mav;
+	}
+	
+	@RequestMapping(value="/reviewCreate.do")
+	public ModelAndView reviewCreate(
+			@RequestParam("orderId") Long orderId,
+			@RequestParam("productId") Long productId,
+			@RequestParam("title") String title,
+			@RequestParam("content") String content,
+			@RequestParam("rating") Double rating,
+			@RequestParam(value = "photos", required = false) List<MultipartFile> photos,
+			HttpSession session
+			) {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		
+		Map<String, Object> param = new HashMap<String, Object>();
+		
+		Map<String, Object> userInfo = (Map<String, Object>) session.getAttribute("userInfo");
+		if(userInfo != null) {
+			String memberId = (String) userInfo.get("MEMBER_ID");
+			param.put("memberId", memberId);
+		}
+		
+        // 리뷰 정보 출력
+        System.out.println("제목: " + title);
+        System.out.println("내용: " + content);
+        System.out.println("별점: " + rating);
+        
+        param.put("orderId", orderId);
+        param.put("productId", productId);
+        param.put("title", title);
+        param.put("content", content);
+        param.put("rating", rating);
+        
+        System.out.println(param);
+        
+        userReviewService.insertReviewByUser(param, photos);
+            
+		return mav;
+	}
+	
+	@RequestMapping(value="/reviewWrite.do")
+	public ModelAndView reviewWrite() {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("review/reviewWrite");
 		
 		return mav;
 	}
